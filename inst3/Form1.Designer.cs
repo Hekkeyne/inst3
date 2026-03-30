46bc495dbdbf74b275347c8241ff39b37b39a350
Данил Митясов <mrstickmananimation@gmail.com>
2026-03-31 00:43:09 +0700

    Добавьте файлы проекта.

---
+++ inst3/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace inst3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(112, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.KeyDown += tb_kp;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(112, 57);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 1;
+            textBox2.KeyDown += tb_kp;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(23, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(83, 45);
+            label1.TabIndex = 2;
+            label1.Text = "Критерий\r\n\r\nАльтернатива";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 102);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Size = new Size(335, 164);
+            dataGridView1.TabIndex = 3;
+            dataGridView1.CellValueChanged += cvchg;
+            dataGridView1.CurrentCellDirtyStateChanged += dataGridView_CurrentCellDirtyStateChanged;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.AllowUserToDeleteRows = false;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView2.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(353, 102);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersVisible = false;
+            dataGridView2.Size = new Size(335, 164);
+            dataGridView2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            button1.Location = new Point(353, 69);
+            button1.Name = "button1";
+            button1.Size = new Size(37, 25);
+            button1.TabIndex = 5;
+            button1.Text = "тык";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
+        private Button button1;
+    }
+}
---
+++ inst3/Form1.cs
@@ -0,0 +1,247 @@
+using System.Data;
+
+namespace inst3
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<Keys, Action<DataGridView>> dict1;
+        public Form1()
+        {
+            InitializeComponent();
+            dict1 = new Dictionary<Keys, Action<DataGridView>>
+        {
+            {Keys.Menu,(x)=>table_schet(x)}
+        };
+        }
+        private void table_schet(DataGridView dtv)
+        {
+            for (int i = 0; i <= dtv.Columns.Count - 1; i++)
+            {
+                for (int j = 1; j <= dtv.Rows.Count; j++)
+                {
+                    dtv[i, j - 1].Value = i == j - 1 ? "*" : dtv[i, j - 1].Value;
+                    dtv[i, j - 1].ReadOnly = true;
+                }
+            }
+        }
+        public int _sbk = 0;
+        public int _str = 1;
+        private void tb_kp(object sender, KeyEventArgs e)
+        {
+            if (dataGridView1.Columns.Count < 2)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    dataGridView1.Columns.Add("", "");
+                }
+                dataGridView1.Rows.Add("*", "Вес", "L", "Flag");
+                _sbk++;
+            }
+            if (sender == textBox2 && e.KeyCode == Keys.Enter)
+            {
+                if (dataGridView1.Columns.Count < 2)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        dataGridView1.Columns.Add("", "");
+                    }
+                    dataGridView1.Rows.Add("*", "Вес", "L", "Flag");
+                    _sbk++;
+                }
+                DataGridViewTextBoxColumn cd = new DataGridViewTextBoxColumn();
+                dataGridView1.Columns.Insert(_sbk, cd);
+                dataGridView1[_sbk, 0].Value = textBox2.Text;
+                dataGridView1[_sbk, 0].ReadOnly = true;
+                _sbk++;
+                addsome(textBox2.Text, textBox2);
+                textBox2.Clear();
+                L_schet();
+            }
+            else if (e.KeyCode == Keys.Enter)
+            {
+                if (dataGridView1.Columns.Count < 2)
+                {
+                    dataGridView1.Columns.Add("", "");
+                    dataGridView1.Rows.Add("*");
+                    _str++;
+                }
+                dataGridView1.Rows.Add();
+                dataGridView1[0, _str].Value = textBox1.Text;
+                dataGridView1[0, _str].ReadOnly = true;
+                _str++;
+                addsome(textBox1.Text, textBox1);
+                textBox1.Clear();
+                flg();
+                L_schet();
+            }
+        }
+        public List<string> krit = new List<string>();
+        public List<string> alter = new List<string>();
+        private void addsome(string a, object sender)
+        {
+            if (sender == textBox1)
+            {
+                krit.Add(a);
+            }
+            if (sender == textBox2)
+            {
+                alter.Add(a);
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                dict1[e.KeyCode](dataGridView1);
+                MessageBox.Show($"{e.KeyCode} нажата");
+            }
+            catch
+            {
+            }
+        }
+        private void flg()
+        {
+            DataGridViewCheckBoxCell cll = new DataGridViewCheckBoxCell();
+            cll.ValueType = typeof(bool);
+            for (int i = 1; i < dataGridView1.Rows.Count; i++)
+            {
+                var ncll = (DataGridViewCheckBoxCell)cll.Clone();
+                ncll.Value = false;
+                dataGridView1[dataGridView1.Columns.Count - 1, i] = ncll;
+            }
+        }
+        private void L_schet()
+        {
+
+            for (int i = 1; i < dataGridView1.Rows.Count; i++)
+            {
+                double max = double.NegativeInfinity;
+                double min = double.PositiveInfinity;
+                for (int j = 1; j < dataGridView1.Columns.Count - 3; j++)
+                {
+                    try
+                    {
+                        max = max < Convert.ToDouble(dataGridView1[j, i].Value) ? Convert.ToDouble(dataGridView1[j, i].Value) : max;
+                        min = min > Convert.ToDouble(dataGridView1[j, i].Value) ? Convert.ToDouble(dataGridView1[j, i].Value) : min;
+                        //MessageBox.Show($"{dataGridView1[j, i].Value}");
+                    }
+                    catch
+                    {
+                        //MessageBox.Show($"{dataGridView1[j, i].Value} {j} {i}");
+                    }
+                }
+                dataGridView1[dataGridView1.Columns.Count - 2, i].Value = max - min;
+            }
+
+        }
+
+        private void dataGridView_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (sender == dataGridView1)
+            {
+                if (dataGridView1.IsCurrentCellDirty)
+                {
+                    dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+                }
+            }
+        }
+
+        private void cvchg(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex >= 1 && e.ColumnIndex < dataGridView1.Columns.Count - 3 && e.RowIndex >= 1 && e.RowIndex < dataGridView1.Rows.Count)
+            {
+                L_schet();
+            }
+        }
+        private bool proverka1()
+        {
+            int user_ves = 0;
+            for (int i = 1; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 1; j < dataGridView1.Columns.Count - 3; j++)
+                {
+                    var c = dataGridView1[j, i].Value;
+                    if (c == null)
+                    {
+                        MessageBox.Show($"Проверьте Столбец {j + 1} и строку {i + 1}");
+                        return false;
+                    }
+
+                }
+            }
+            for (int i = 1; i < dataGridView1.Rows.Count; i++)
+            {
+                try
+                {
+
+                    user_ves = user_ves + Convert.ToInt32(dataGridView1[dataGridView1.Columns.Count - 3, i].Value);
+                }
+                catch { MessageBox.Show($"столбец {dataGridView1.Columns.Count - 3} строка {i} странное значение"); return false; }
+            }
+            return true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!proverka1())
+            {
+                return;
+            }
+            int ii = 1;
+            dataGridView2.Columns.Add("", "");
+            dataGridView2.Rows.Add("*");
+            foreach (string a in alter)
+            {
+                dataGridView2.Columns.Add("", "");
+                dataGridView2[ii, 0].Value = a;
+                dataGridView2[ii, 0].ReadOnly = true;
+                dataGridView2.Rows.Add(a);
+                ii++;
+            }
+            table_schet(dataGridView2);
+            int ves = 0;
+            for (int i = 1; i < dataGridView1.Rows.Count; i++)
+            {
+                ves = ves + Convert.ToInt32(dataGridView1[dataGridView1.Columns.Count - 3, i].Value);
+            }
+            int totalves = 0;
+            for (int i = 1; i < dataGridView2.Rows.Count;i++)
+            {
+                for( int j =1; j < dataGridView2.Columns.Count; j++)
+                {
+                    if (dataGridView2[j, i].Value != "*")
+                    {
+                        for ( int y = 1; y < dataGridView1.Rows.Count; y++)
+                        {
+                            if (dataGridView1[dataGridView1.Columns.Count-1,y].Value is bool bl && bl)
+                            {
+                                if (Convert.ToInt32(dataGridView1[j, y].Value) < Convert.ToInt32(dataGridView1[i, y].Value))
+                                {
+                                    totalves += Convert.ToInt32(dataGridView1[dataGridView1.Columns.Count - 3, y].Value);
+                                }
+                            }
+                            if (dataGridView1[dataGridView1.Columns.Count - 1, y].Value is bool b && !b)
+                            {
+                                if (Convert.ToInt32(dataGridView1[j, y].Value) >= Convert.ToInt32(dataGridView1[i, y].Value))
+                                {
+                                    totalves += Convert.ToInt32(dataGridView1[dataGridView1.Columns.Count - 3, y].Value);
+                                }
+                            }
+                        }
+                        dataGridView2[j, i].Value = (double)totalves / ves;
+                        totalves = 0;   
+                    }
+                }
+            }
+            MessageBox.Show($"{dataGridView2.Rows.Count} {dataGridView2.Columns.Count}Теперь нельзя изменять критерии и альтернативы\n\rНажмите заново, если необходимо что-то изменить");
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            dataGridView1.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
